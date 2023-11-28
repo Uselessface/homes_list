92e5572e2e2fcc64d1cced551f2eb4dbbf452122
Uselessface <60576572+Uselessface@users.noreply.github.com>
2023-11-28 16:24:36 +0400

    fff

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {HomeComponent} from "./home/home.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    HomeComponent,
+  ],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img src=".././assets/logo.svg" alt="logo" class="brand-logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'homes';
+}
